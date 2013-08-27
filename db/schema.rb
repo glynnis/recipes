99897e73a26ec04e868f9b176302f4df7df2da42
Glynnis Ritchie <glynnish@gmail.com>
2013-08-27 11:40:04 -0500

    Create recipes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130827150344) do
+ActiveRecord::Schema.define(version: 20130827163728) do
+
+  create_table "recipes", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
