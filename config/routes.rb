a12fa9aeed3d00f1ae0603abfbcea2ec9ebd455b
Glynnis Ritchie <glynnish@gmail.com>
2013-08-28 10:46:40 -0500

    WIP: add devise users

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Recipes::Application.routes.draw do
+  devise_for :users
   resources :users
 
   root 'home#home'
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # devise routes
+  # devise_for :user, 
+  # :as => '', 
+  # :path_names => { 
+  #   :sign_in => "login", 
+  #   :sign_out => "logout", 
+  #   :sign_up => "register" 
+  # }
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
